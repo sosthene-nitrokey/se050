1c0c123d6628cd06c5df367256ff88752092f2b7
Robert <robert.stojic@ptb.de>
2023-03-13 17:20:07 +0100

    work

--- src/se050.rs
+++ src/se050.rs
@@ -1768,7 +1768,7 @@ where
     fn generate_p256_key(&mut self, delay: &mut DelayWrapper) -> Result<ObjectId, Se050Error> {
        // let tlv1 = SimpleTlv::new(Se050TlvTag::Tag1.into(), &[0xae, 0x51, 0xae, 0x51]);
       //    let tlv1 = SimpleTlv::new(Se050TlvTag::Tag1.into(), &[0x20, 0xE8, 0xA0, 0x01]);
-      let tlv1 = SimpleTlv::new(Se050TlvTag::Tag1.into(), &[ 0x01, 0xA1,  0xE8, 0x20] );
+      let tlv1 = SimpleTlv::new(Se050TlvTag::Tag1.into(), &[ 0x02, 0xA1,  0xE8, 0x20] );
     //let tlv1 = SimpleTlv::new(Se050TlvTag::Tag1.into(), &[ 0x05, 0xA0,  0xE8, 0x20] );
         let tlv2 = SimpleTlv::new(Se050TlvTag::Tag2.into(), &[0x03]);	// NIST P-256
         let mut capdu = CApdu::new(
@@ -1795,7 +1795,7 @@ where
         }
 
         debug!("SE050 GenP256 OK");
-        Ok(ObjectId([ 0x01, 0xA1,  0xE8, 0x20]))
+        Ok(ObjectId([ 0x02, 0xA1,  0xE8, 0x20]))
     }
   
  
