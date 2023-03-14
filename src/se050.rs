a1a7a1595cf26429a5339820200366304487d09a
Robert <robert.stojic@ptb.de>
2023-03-14 14:39:49 +0100

    work

--- src/se050.rs
+++ src/se050.rs
@@ -1760,7 +1760,7 @@ where
     /* NOTE: hardcoded Object ID 0xae51ae51! */
     fn generate_p256_key(&mut self, delay: &mut DelayWrapper) -> Result<ObjectId, Se050Error> {
         
-         let tlv1 = SimpleTlv::new(Se050TlvTag::Tag1.into(), &[0xae, 0x51, 0xae, 0x51] );
+         let tlv1 = SimpleTlv::new(Se050TlvTag::Tag1.into(), &[0xae, 0x52, 0xae, 0x52] );
        
         let tlv2 = SimpleTlv::new(Se050TlvTag::Tag2.into(), &[0x03]);	// NIST P-256
         
@@ -1792,7 +1792,7 @@ where
 
         debug!("SE050 GenP256 OK");
 
-        Ok(ObjectId([0xae, 0x51, 0xae, 0x51]))
+        Ok(ObjectId([0xae, 0x52, 0xae, 0x52]))
        // Ok(ObjectId([ 0x99, 0xA0, 0xE8, 0x20]))
       
 
