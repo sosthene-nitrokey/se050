f8233798fb974afc45a606af71fa096678b582c8
Robert <robert.stojic@ptb.de>
2023-03-28 16:39:22 +0200

    var

--- src/se050.rs
+++ src/se050.rs
@@ -6298,12 +6298,12 @@ fn generate_ed255_key_pair(&mut self, objectidentifier: &[u8;4] ,delay: &mut Del
     let tlv1_ret = rapdu.get_tlv(Se050TlvTag::Tag1.into()).ok_or_else(|| {
         error!("Se050 crate: SE050 Check object exist TLV Missing");
         Se050Error::UnknownError })?;
-
+/*  
     if tlv1_ret.get_data().len() != buf.len() {
         error!("Se050 crate:  SE050 Check object exist TLV Length Mismatch");
         return Err(Se050Error::UnknownError);
     }
-
+*/
     buf.copy_from_slice(tlv1_ret.get_data());
 
 
